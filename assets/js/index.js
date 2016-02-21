1ebe423064709d2ab4d6b3182bb2fa017a051a4c
Dan Zheng <zheng321@purdue.edu>
2016-02-20 22:13:29 -0600

    Lots of small changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,1 @@
-$(function(){$(".tlt").textillate({minDisplayTime:1500,"in":{effect:"fadeInLeft",sync:!1},out:{delay:3,effect:"fadeOut",sync:!1},loop:!0})});
+$(function(){$(".tlt").textillate({minDisplayTime:1500,initialDelay:500,"in":{effect:"fadeInLeft",sync:!1},out:{delay:3,effect:"fadeOut",sync:!1},loop:!0})});
